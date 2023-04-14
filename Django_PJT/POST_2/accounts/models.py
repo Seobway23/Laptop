90b5c0d0ed8ca016d41d2dfde2e74ceb267f02db
이성섭 <ljs8828@naver.com>
2023-04-14 18:02:58 +0900

    2

--- Django_PJT/POST_2/accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class User(AbstractUser):
-    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
-    pass
-# 따로 유저를 만들 필요가 없는건지? FK 잘 모르겠음
