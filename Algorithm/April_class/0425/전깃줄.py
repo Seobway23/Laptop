38cd7fd6df808e2c42f3a9605c5e0fb317b7e69a
이성섭 <ljs8828@naver.com>
2023-04-27 09:46:42 +0900

    0427 algo

--- Algorithm/April_class/0425/전깃줄.py
+++ Algorithm/April_class/0425/전깃줄.py
@@ -1,25 +1,59 @@
 import sys
 sys.stdin= open('input.txt', 'r')
 
+'''
+# 1 dp 배열 인덱스 판별
 N = int(input())
-v = [0]*501
-ans = []
+
+# 필요한 배열 선언
+lst = []
+dp = [0]*(N+1)
+
+
+# input 받기
 for _ in range(N):
     A, B = map(int, input().split())
-    v[A] = B
+    lst.append((A, B))
 
-for i in range(1, 501):
-    if v[i] !=0:
-        ans.append(v[i])
+# 오름차순 정렬
+lst.sort()
 
-print(ans)
+# 2차원 배열 순회
+for i in range(N):
+    sm = 0
+    # 지금 배열과 비교했을 때 크면 sm ++
+    for j in range(i, N):
+        if lst[i][1] < lst[j][1]:
+            sm += 1
+    # dp 배열 안에 넣기
+    dp[i] = sm
 
-dp = [1 for i in range(len(ans))]
-for j in range(len(ans)):
-    for k in range(j):
-        if ans[j] > ans[k]:
-            dp[j] = max(dp[j], dp[k]+1)
+print(N-max(dp))
 
-print(len(ans) - max(dp))
+틀렸음 이유는 모르겠음
+'''
+
+# 1 dp 배열 누적합
+N = int(input())
+
+# 필요한 배열 선언
+lst = []
+dp = [0]*(N)
 
 
+# input 받기
+for _ in range(N):
+    A, B = map(int, input().split())
+    lst.append((A, B))
+
+# 정렬을 안해서 틀렸네 ...
+lst.sort()
+
+for j in range(N): # 큰 수
+    for i in range(j): # 작은 수
+        if lst[i][1] < lst[j][1] and dp[i] > dp[j]:
+            dp[j] = dp[i]
+
+    dp[j] += 1
+
+print(N - max(dp))
