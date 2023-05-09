32a56ba98c9ddf5c8270333b0e756f2e08c1f293
이성섭 <ljs8828@naver.com>
2023-05-09 16:58:38 +0900

    0509

---
+++ Vue/practice4_lotto_pick_lunch/views/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>로또</h1>
+    <button @click="getLucky"> Get Lucky Numbers</button>
+    <p>{{numbers}}</p>
+  </div>
+</template>
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'getLucky',
+  data(){
+    return{
+      numbers:null,
+    }
+  },
+  methods:{
+    getLucky(){
+      const number = _.sampleSize(_.range(1,46),6)
+      this.numbers = number
+    }
+  }
+
+}
+</script>
---
+++ Vue/practice4_lotto_pick_lunch/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>점심메뉴</h1>
+    <button @click="pickLunch"> Pick Lunch</button>
+    <p>{{lunch}}</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  data(){
+    return{
+      lunch:null,
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods:{
+    pickLunch(){
+      const lunchOptions = ['Pizza', 'Burger', 'Salad', 'Sushi']
+      const randomIndex = Math.floor(Math.random() * lunchOptions.length)
+      this.lunch = lunchOptions[randomIndex]
+    }
+  }
+}
+</script>
