d2324ba8793cf227896bfa34349d581c4e3d61b7
이성섭 <122852052+Seobway23@users.noreply.github.com>
2024-01-25 21:16:23 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,10 +1,12 @@
 n = int(input())
-lst = [0] * (n + 1)  # 계단 점수 리스트의 크기를 n+1로 설정
+lst = [0] * (n + 1)     # 계단 점수 리스트 크기: n + 1
 for i in range(1, n + 1):
     lst[i] = int(input())
 
-dp = [0] * (n + 1)  # dp 리스트의 크기를 n+1로 설정
+dp = [0] * (n + 1)      # dp 리스트 크기: n + 1
 dp[1] = lst[1]
+
+#1일 경우도 고려해야 하기 때문에 if 문 추가
 if n > 1:
     dp[2] = lst[1] + lst[2]
 
