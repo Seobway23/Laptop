83e049ad7edf49fb9ec52b486f0bdb4ab27d2e0a
이성섭 <122852052+Seobway23@users.noreply.github.com>
2024-09-06 09:16:03 +0900

    [Silver III] Title: 반복하지 않는 수, Time: 276 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.cc
+++ 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.cc
@@ -3,38 +3,39 @@
 
 using namespace std;
 
-bool findUnique(int num) {
-    bool digits[10] = {false}; // 각 자릿수를 추적하는 배열
+// unique 인지 판별하는 함수
+bool isUnique(int num) {
+    bool arr[10] = {false};
 
-    while (num > 0) {
-        int digit = num % 10;
-        if (digits[digit]) {
-            return false; // 중복된 자릿수 발견
+    while (num >0) {
+        int target =  num % 10;
+        if (arr[target]) {
+            return false;
         }
-        digits[digit] = true;
+        arr[target] = true;
         num /= 10;
     }
     return true;
 }
 
 int main() {
-    vector<int> nums;
-    nums.reserve(1000000);
 
-    for (int i = 1; nums.size() < 1000000; i++) {
-        if (findUnique(i)) {
-            nums.push_back(i);
+    // 연속X 리스트 만들기
+    vector<int> uniqueList;
+    uniqueList.reserve(1000000);
+
+    for(int i = 1; uniqueList.size() < 1000000; i++) {
+        if (isUnique(i)) {
+            uniqueList.push_back(i);
         }
     }
-
     int n;
     while (true) {
         cin >> n;
-        if (n == 0) {
-            break;
-        }
-        cout << nums[n - 1] << endl; // 미리 계산된 값에서 인덱스 접근
-    }
 
-    return 0;
+        if (n==0) {
+            return 0;
+        }
+        cout << uniqueList[n-1]<< endl;
+    }
 }
