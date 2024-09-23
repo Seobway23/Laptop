2b07675569819f9c9d1d0a77f23ff4a2625a7b47
이성섭 <122852052+Seobway23@users.noreply.github.com>
2024-09-23 12:29:59 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.05 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int ans = -1;
+        for ( int i=0; i<num_list.length; i++){
+            if (num_list[i] < 0){
+                ans = i;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+}
