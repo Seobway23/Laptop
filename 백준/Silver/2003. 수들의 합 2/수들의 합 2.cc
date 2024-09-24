5a0cb17f84d38bb36d81d4450761841cae2af77c
이성섭 <122852052+Seobway23@users.noreply.github.com>
2024-09-24 14:05:40 +0900

    [Silver IV] Title: 수들의 합 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -1,34 +1,38 @@
 #include <iostream>
-using namespace std;
 
+using namespace std;
 int main() {
+    // 시간 제한 0.5초 => 최적화 시켜라!
+    // 1. input
     int n, m;
     cin >> n >> m;
-    
+
     int arr[n];
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    // 투포인터 초기화
-    int start = 0, end = 0;
-    int current_sum = 0, count = 0;
-    
-    while (end <= n) {
-        if (current_sum < m) {
-            if (end == n) break;
-            current_sum += arr[end];
-            end++;
-        } else if (current_sum > m) {
-            current_sum -= arr[start];
-            start++;
-        } else { // current_sum == m
-            count++;
-            current_sum -= arr[start];
-            start++;
+    int s = 0, e = 0;
+    int cur_sum = 0, cnt = 0;
+
+    while (e <= n) {
+        if (cur_sum < m) {
+            if (e == n) break;
+            cur_sum += arr[e];
+            e++;
+        }
+        else if (cur_sum > m ) {
+            cur_sum -= arr[s];
+            s++;
+        }
+        else { // 같을 때
+            cnt++;
+            cur_sum -= arr[s];
+            s++;
         }
     }
 
-    cout << count << endl;
+    cout << cnt << endl;
+
     return 0;
 }
