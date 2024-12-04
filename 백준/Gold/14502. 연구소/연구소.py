4aadc9afd5024084464e23125dd4dfaf2a8b9bbd
이성섭 <122852052+Seobway23@users.noreply.github.com>
2024-12-04 17:32:49 +0900

    [Gold IV] Title: 연구소, Time: 420 ms, Memory: 115028 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,82 +1,46 @@
-
 from collections import deque
+from itertools import combinations
 
-# dfs
-def dfs(si, sj, depth, lst, v):
+# bfs 함수는 기존과 동일
+def bfs():
     new_arr = [row[:] for row in arr]
-    # 종료 조건
-    if depth > 3:
-        return
-
-    if len(lst) == 3:
-        all_lst.append(lst[:])
-        return
-
-    for i in range(si, n):
-        for j in range( m):
-            if not arr[i][j] and not v[i][j]:
-                # v, lst 추가
-                v[i][j] = 1
-                lst.append((i, j))
-                dfs(i, j, depth + 1, lst, v)
-                # v, lst 제거
-                lst.pop()
-                v[i][j] = 0
-
-# bfs
-def bfs(new_arr):
-    v = [[0] * m for _ in range(n)]
+    q = deque()
     for i in range(n):
         for j in range(m):
             if new_arr[i][j] == 2:
-                q = deque()
                 q.append((i, j))
-                v[i][j] = 1
-                while q:
-                    ti, tj = q.popleft()
 
-                    # 4방향 탐색
-                    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-                        ni, nj = ti + di, tj + dj
-
-                        # 미방문, 0일 때, 2 추가
-                        if 0 <= ni < n and 0 <= nj < m and not new_arr[ni][nj]:
-                            new_arr[ni][nj] = 2
-                            q.append((ni, nj))
-                            v[ni][nj] = 1
-    return new_arr
+    while q:
+        ti, tj = q.popleft()
+        for di, dj in [(-1,0),(1,0),(0,-1),(0,1)]:
+            ni, nj = ti + di, tj + dj
+            if 0 <= ni < n and 0 <= nj < m:
+                if new_arr[ni][nj] == 0:
+                    new_arr[ni][nj] = 2
+                    q.append((ni, nj))
+    cnt = sum(row.count(0) for row in new_arr)
+    return cnt
 
 # input
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 # init
-v = [[0] * m for _ in range(n)]
+empty_positions = [(i, j) for i in range(n) for j in range(m) if arr[i][j] == 0]
 ans = 0
-all_lst = []
 
-# 3개 선정
-for i in range(n):
-    for j in range(m):
-        if not arr[i][j] and not v[i][j]:
-            v[i][j] =1
-            # dfs 플래그는 탈출을 위해 n + 1 turn 을 무조건 해야 함
-            dfs(i, j, 1, [(i, j)], v)
-            v[i][j] = 0
-
-# 가장 최적 찾기
-for lst in all_lst:
-    new_arr = [row[:] for row in arr]
-
-    # 벽 3개 세우기
-    for i, j in lst:
-        new_arr[i][j] = 1
+# 3 walls combi
+for walls in combinations(empty_positions, 3):
+    # 벽 설치
+    for i, j in walls:
+        arr[i][j] = 1
 
     # 바이러스 전파
-    new_arr = bfs(new_arr)
-    cnt = sum(1 for row in new_arr for cell in row if cell == 0)
+    safe_area = bfs()
+    ans = max(ans, safe_area)
 
-    # answer 갱신
-    ans = max(ans, cnt)
+    # 벽 제거
+    for i, j in walls:
+        arr[i][j] = 0
 
 print(ans)
