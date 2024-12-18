61bb782d111c2f97f39366a8e3d8c4d101529447
이성섭 <122852052+Seobway23@users.noreply.github.com>
2024-12-18 10:30:17 +0900

    [level 0] Title: 이어 붙인 수, Time: 31.50 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String A = "";
+        String B = "";
+        for (int i =0; i < num_list.length; i++){
+            if (num_list[i] % 2 == 0) { // 짝수일 경우
+                A += num_list[i];
+            }
+            
+            else {
+                B += num_list[i];
+            }
+        }
+        
+        answer = Integer.parseInt(A) + Integer.parseInt(B);
+        return answer;
+    }
+}
