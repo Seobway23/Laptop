e19d2994ddcb005950fcb559cd14486961c169c9
이성섭 <122852052+Seobway23@users.noreply.github.com>
2025-07-03 19:56:38 +0900

    [Gold V] Title: 토마토, Time: 328 ms, Memory: 163852 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,42 +1,49 @@
 from collections import deque
 
-m, n = map(int, input().split())
+# m: col, n: row
+m,n = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-di, dj = [0, 0, -1, 1], [-1, 1, 0, 0]
-visited = [[0] * m for _ in range(n)]
 
-start_to = deque()
-# start_to 찾기
+# 1을 먼저 찾기
+start = []
 for i in range(n):
     for j in range(m):
-        if arr[i][j] == 1 and visited[i][j] == 0:
-            start_to.append([i, j])
-            visited[i][j] = 1
+        if arr[i][j] == 1:
+            start.append((i, j))
 
-# 익은거 BFS 돌리기
-info = -1
-while start_to:
-    info += 1
-    for _ in range(len(start_to)):
-        ti, tj = start_to.popleft()
 
-        # 4 방향 탐색,
-        for k in range(4):
-            ni, nj = ti + di[k], tj + dj[k]
+q = deque(start)
+cnt = 0
 
-            # 범위내, 미방문 일 때
-            if 0 <= ni < n and 0 <= nj < m and arr[ni][nj] == 0 and visited[ni][nj] == 0:
-                start_to.append([ni, nj])
-                visited[ni][nj] = 1
+while q:
+    flag = 0
+    for _ in range(len(q)):
+        ti, tj = q.popleft()
+
+        # 4방향 탐색
+        for di, dj  in ((0, 1), (0, -1), (-1, 0), (1, 0)):
+            ni, nj = ti + di, tj + dj
+
+            if 0 <= ni < n and 0 <= nj < m and arr[ni][nj] == 0:
+                q.append((ni, nj))
                 arr[ni][nj] = 1
+                flag = 1
 
-# 정답 확인
-ans, cnt = -1, 0
+    if flag == 1:
+        cnt += 1
+
+
+zero = 0
 for i in range(n):
     for j in range(m):
-        if arr[i][j] != 0:
-            cnt += 1
-if cnt == m*n:
-    ans = info
+        if arr[i][j] == 0:
+            zero = 1
+            break
 
-print(ans)
+if zero == 1:
+    print(-1)
+
+else:
+    print(cnt)
+
+
