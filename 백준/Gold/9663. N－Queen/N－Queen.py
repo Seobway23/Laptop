4bf5d60e62555dc2cdd8e4068baa4943c3594e87
이성섭 <122852052+Seobway23@users.noreply.github.com>
2025-07-15 19:59:15 +0900

    [Gold IV] Title: N-Queen, Time: 6660 ms, Memory: 120036 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,29 +1,25 @@
 # 입력
 n = int(input())
 
-# 초기값
-ans = 0
-row = [0] * n
-
-def find(ci):
-    for i in range(ci):
-        if row[ci] == row[i] or abs(row[ci] - row[i]) == abs(ci - i):
-            return False
-
-    return True
-
 def dfs(i):
     global ans
+    # 종료 조건
     if i == n:
-        ans +=1
+        ans += 1
         return
 
-    else:
-        for j in range(n):
-            row[i] = j
-            if find(i):
-                dfs(i + 1)
+    # 가로 X, 대각선 아래 X, 대각선 위 X
+    for j in range(n):
+        if v1[j] == v2[i + j] == v3[i - j] == 0:
+            v1[j] = v2[i + j] = v3[i - j] = 1
+            dfs(i + 1)
+            v1[j] = v2[i + j] = v3[i - j] = 0
 
+
+# init
+ans = 0
+v1 = [0]* n # 가로
+v2 = [0] * (2*n) # 대각선 위
+v3 = [0] * (2 * n) # 대각선 아래
 dfs(0)
 print(ans)
-
