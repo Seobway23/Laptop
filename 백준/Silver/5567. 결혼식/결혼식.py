c349ac5c4fce82fca3f0e00b5a649c67ea9111fa
이성섭 <122852052+Seobway23@users.noreply.github.com>
2025-07-20 22:13:24 +0900

    [Silver II] Title: 결혼식, Time: 140 ms, Memory: 114100 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.py
+++ 백준/Silver/5567. 결혼식/결혼식.py
@@ -3,19 +3,25 @@ from collections import deque
 n = int(input())
 m = int(input())
 arr = [[0] * n for _ in range(n)]
-v = [0]
-
 for _ in range(m):
     n1, n2 = map(int, input().split())
-    arr[n1-1][n2-1] , arr[n2-1][n1-1] = 1, 1
+    arr[n1-1][n2-1] = arr[n2-1][n1-1] =  1
 
-q = deque([0])
-for _ in range(2):
-    for _ in range(len(q)):
-        ci = q.popleft()
-        for j in range(n):
-            if  arr[ci][j] == 1:
-                q.append(j)
-                v.append(j)
+invite = set()
+q = deque([(0, 0)])
+visited = [[0] * n  for _ in range(3)] # v[i][j] -> 깊이 d에서 j를 처리 했는지
+visited[0][0] = 1
 
-print(len(set(v)) - 1)
+while q:
+    ci, depth = q.popleft()
+    if depth == 2:
+        continue
+
+    for j in range(n):
+        if arr[ci][j] == 1 and visited[depth + 1][j] == 0:
+            visited[depth + 1][j] = 1
+            if j != 0:
+                invite.add(j)
+            q.append((j, depth + 1))
+
+print(len(invite))
