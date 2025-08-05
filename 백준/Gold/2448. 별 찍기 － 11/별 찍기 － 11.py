58cd648a9e11d7c93ad4db06d23daac167a51f48
이성섭 <122852052+Seobway23@users.noreply.github.com>
2025-08-05 09:12:33 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 136 ms, Memory: 160952 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -2,10 +2,13 @@ def fractal(n):
     if n == 3:
         return ["  *  ", " * * ", "*****"]
 
-    prev = fractal(n // 2)
-    top_1 = [ " " * (n // 2) + line +" " * (n // 2) for line in prev]
-    bottom_2 = [line + " " + line for line in prev]
-    return top_1 + bottom_2
+    next_num = n // 2
+    prev = fractal(next_num)
+    # top은 next_num 만큼 빈칸을 양 옆으로 띄워주면 됨
+    top = [" " * (next_num) + line +  " " * (next_num)  for line in prev]
+    # bottom은 line 과 line 사이에 " " 공백 하나만 있으면 됨
+    bottom = [line + " " + line for line in prev]
+    return top + bottom
 
 
 n = int(input())
