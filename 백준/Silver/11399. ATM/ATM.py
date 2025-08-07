6d4c992544674c2016e3940aec44c7431a80aee1
이성섭 <122852052+Seobway23@users.noreply.github.com>
2025-08-07 15:21:15 +0900

    [Silver IV] Title: ATM, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,10 +1,12 @@
 n = int(input())
-lst = list(map(int, input().split()))
-lst.sort()
-
+arr = list(map(int, input().split()))
+arr.sort()
 ans = 0
-for i in range(1, n + 1):
-    for ci in range(i):
-        ans += lst[ci]
+mul = n
+for j in arr:
+    ans += mul * j
+    mul -= 1
+
+
 
 print(ans)
