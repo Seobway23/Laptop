7bff1f7bc55e6a676be57feb9223d060b53b4b9d
이성섭 <122852052+Seobway23@users.noreply.github.com>
2026-03-10 20:27:04 +0900

    [Gold III] Title: 경사로, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.py
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -1,47 +1,50 @@
-def verify(lst):
-    v = [0] * n
-    flag = [0] * n
-    for i in range(n-1):    # i, i + 1 이므로 n-2까지만 해야 함
-        # 2 이상 False
-        if abs(lst[i] - lst[i + 1]) > 1:
+import sys
+input = sys.stdin.readline
+
+# 1. input
+n, l = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+cnt = 0
+
+# 2. capa():
+def capa(line):
+    used = [False] * n
+
+    for i in range(n - 1):
+        if line[i] == line[i + 1]:
+            continue
+
+        # 높이 차이가 2 이상
+        if abs(line[i] - line[i + 1]) > 1:
             return False
 
-        # 1 미만 일 때      # 3 3 3 3 2 2 일 때  for 문 돌려서 v 가 아닐 때 는
-        else:
-            # 내리막
-            next, cur = lst[i + 1], lst[i]
-            if cur > next:
-                for j in range(1, L + 1):
-                    ni = i + j
-                    if 0<= ni < n and next == lst[ni] and not v[ni]: # 범위내, 미방문, 조건1
-                        v[ni] = 1   # next 에 v = 1 해야 함
-                        # print("", i,v)
+        # 오르막: line[i] < line[i+1]
+        if line[i] + 1 == line[i + 1]:
+            h = line[i]
+            for j in range(i, i - l, -1):
+                if j < 0 or line[j] != h or used[j]:
+                    return False
+                used[j] = True
 
-                    else:
-                        return False
-
-            # 오르막
-            elif cur < next:
-                for j in range(0, L):
-                    ni = i - j
-                    if 0<= ni < n and cur == lst[ni] and not v[ni]: # 범위내, 미방문, 조건1
-                        v[ni] = 1    # cur 에 v = 1 해야 함
-                    else:
-                        return False
+        # 내리막: line[i] > line[i+1]
+        elif line[i] - 1 == line[i + 1]:
+            h = line[i + 1]
+            for j in range(i + 1, i + l + 1):
+                if j >= n or line[j] != h or used[j]:
+                    return False
+                used[j] = True
 
     return True
 
-n, L = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-ans = 0
+# 3. iterator
+for i in range(n):
+    if capa(arr[i]):   # 행
+        cnt += 1
 
-# 가로
-for lst in arr:
-    if verify(lst):
-        ans += 1
+for j in range(n):
+    col = [arr[i][j] for i in range(n)]   # 열
+    if capa(col):
+        cnt += 1
 
-for lst in list(zip(*arr)):
-    if verify(lst):
-        ans += 1
 
-print(ans)
+print(cnt)
